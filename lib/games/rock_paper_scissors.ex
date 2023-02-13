efd04f2a8856b6bf6a58d66b5612dfb70ebeaa9b
johnhitz203 <johnhitz203@gmail.com>
2023-02-13 16:43:32 -0600

    refactored without atoms

--- lib/games/rock_paper_scissors.ex
+++ lib/games/rock_paper_scissors.ex
@@ -1,6 +1,6 @@
 defmodule Games.RockPaperScissors do
   def play do
-    ai_guess = Enum.random([:rock, :paper, :scissors])
+    ai_guess = Enum.random(["rock", "paper", "scissors"])
     IO.inspect(ai_guess, label: "ai_guess")
 
     IO.inspect(binding())
@@ -8,12 +8,13 @@ defmodule Games.RockPaperScissors do
     guess =
       IO.gets("Pick rock, paper, scissors ")
       |> String.trim()
-      |> String.to_atom()
+
+    # |> String.to_atom()
 
     case {guess, ai_guess} do
-      {:paper, :rock} -> "player wins with paper covers rock!"
-      {:scissors, :paper} -> "Player wins with scissors cuts paper!"
-      {:rock, :scissors} -> "player wins with rock breaks scissors"
+      {"paper", "rock"} -> "player wins with paper covers rock!"
+      {"scissors", "paper"} -> "Player wins with scissors cuts paper!"
+      {"rock", "scissors"} -> "player wins with rock breaks scissors"
       {guess, guess} -> "tie"
       _winner -> "You loser!"
     end
